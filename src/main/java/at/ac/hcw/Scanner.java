3d6fa2e078d324e7b99d6bc64ef731bf3726441d
Jisus14 <alexji0403@gmail.com>
2025-10-09 19:36:07 +0200

    Ping IP v1

--- src/main/java/at/ac/hcw/Scanner.java
+++ src/main/java/at/ac/hcw/Scanner.java
@@ -8,10 +8,24 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 
 public class Scanner extends Application {
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static boolean pingHost(String host, int port, int timeout) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(host, port), timeout);
+            return true;
+        } catch (IOException e) {
+            System.err.println("Could not connect to :" + port + e.getMessage());
+            return false; // Either timeout or unreachable or failed DNS lookup.
+        }
     }
 
     @Override
@@ -23,7 +37,22 @@ public class Scanner extends Application {
 
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                int[] results = new int[500];
+                for (int i = 0; i < 500; i++){
+                    boolean result = pingHost("localhost", i, 20000);
+                    if(result){
+                        results[i] = i;
+                    }
+                    System.out.println(i + ":" + result);
+                }
+
+                for (int i = 0; i < results.length; i++){
+                    if(results[i] != 0){
+                        System.out.println(results[i]);
+                    }
+
+                }
+
             }
         });
 
