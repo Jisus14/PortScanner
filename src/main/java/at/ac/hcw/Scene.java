049574124d07dff4a1b22713a242b9adfe6ebc82
Alexander <alexji0403@gmail.com>
2025-10-20 23:01:40 +0200

    Multithreding added. Textbox for hostname added. Freeze of UI after clicking the button fixed. Gets open ports from ScannerApplication and returns in console.

--- src/main/java/at/ac/hcw/Scene.java
+++ src/main/java/at/ac/hcw/Scene.java
@@ -2,11 +2,17 @@ package at.ac.hcw;
 
 import at.ac.hcw.model.*;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 
 public class Scene extends Application {
@@ -15,29 +21,79 @@ public class Scene extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Port Scanner");
         Button btn = new Button();
+        TextField hostInput = new TextField();
+        hostInput.setPromptText("Enter host or IP address");
         btn.setText("Scan Port");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        //When button is presses
+        btn.setOnAction(event -> {
+            btn.setDisable(true);
+            btn.setText("Scanning...");
 
-            @Override
-            public void handle(ActionEvent event) {
-                //For async scanning
-                int numOfThreads = 20;
-                String host = "localhost";
-                int portStart = 0;
-                int portEnd = 10000;
-                int timeout = 200;
-                int portsPerThread = ((portEnd - portStart) / numOfThreads) + 1; //Plus 1 maybe of rounding loss
-                Thread[] threads = new Thread[numOfThreads];
+            //Temp vars because no UI yet
+            int numOfThreads = 500;
+            int portStart = 0;
+            int portEnd = 65535;
+            int timeout = 200;
 
-                for (int i = 0; i < threads.length; i++) {
-                    threads[i] = new Thread(new ScannerApplication(host,portStart + ((i*portsPerThread)), portStart + ((i+1)*portsPerThread), timeout));
-                    threads[i].start();
+            //Not this
+            String host = hostInput.getText();
+            int portsPerThread = ((portEnd - portStart) / numOfThreads) + 1; //Plus 1 maybe of rounding loss
+
+
+            //Splits work into multiple workers.
+            //2 Arrays so they are targetable for summerization of open ports
+            Thread[] threads = new Thread[numOfThreads];
+            ScannerApplication[] scanners = new ScannerApplication[numOfThreads];
+
+            //Starts loop and gives every worker a set of ports depending on how many threads etc.
+            for (int i = 0; i < threads.length; i++) {
+                int start = portStart + ((i * portsPerThread));
+                int end = start + portsPerThread;
+
+                //Makes sure no false ports are scanned
+                if (end > portEnd) {
+                    end = portEnd;
                 }
+                if (start < portStart) {
+                    start = portStart;
+                }
+                scanners[i] = new ScannerApplication(host, start, end, timeout);
+                threads[i] = new Thread(scanners[i]);
+                threads[i].start();
             }
-        });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+            //New Thread otherwise UI freeze
+            new Thread(() -> {
+                System.out.println("Scanning: " + host);
+
+                List<Integer> allOpenPorts = new ArrayList<>();
+
+                //Collects all open ports from each scanner to allOpenPorts
+                for (int i = 0; i < threads.length; i++) {
+                    try {
+                        threads[i].join();
+                        allOpenPorts.addAll(scanners[i].getOpenPorts());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                //Sorts and prints the list
+                Collections.sort(allOpenPorts);
+                System.out.println(allOpenPorts);
+                //Needs this otherwise error if not with runLater because this is in a thread
+                Platform.runLater(() -> {
+                    btn.setDisable(false);
+                    btn.setText("Done, do again?");
+                });
+            }).start();
+        }
+        );
+
+        VBox root = new VBox(10); // 10px spacing
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(10));
+        root.getChildren().addAll(hostInput, btn);
         primaryStage.setScene(new javafx.scene.Scene(root, 300, 250));
         primaryStage.show();
     }
