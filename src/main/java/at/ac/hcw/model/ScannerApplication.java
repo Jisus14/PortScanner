ddeb35cd34d9154ad6d3a2c8bbb0f0da0b26ac79
Alexander <alexji0403@gmail.com>
2025-10-20 23:03:09 +0200

    Made with Runnable so it works with multithreding (last commit). Function added so main function can get open ports.

--- src/main/java/at/ac/hcw/model/ScannerApplication.java
+++ src/main/java/at/ac/hcw/model/ScannerApplication.java
@@ -3,13 +3,19 @@ package at.ac.hcw.model;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ScannerApplication implements Runnable {
 
-    String host;
-    int portStart;
-    int portEnd;
-    int timeout;
+    //vars
+    private final String host;
+    private final int portStart;
+    private final int portEnd;
+    private final int timeout;
+    private final List<Integer> openPorts = new ArrayList<>();
+
+    //const
     public ScannerApplication(String host, int portStart, int portEnd, int timeout) {
         this.host = host;
         this.portStart = portStart;
@@ -17,58 +23,30 @@ public class ScannerApplication implements Runnable {
         this.timeout = timeout;
     }
 
+    //Main funtion of Runnable
     public void run() {
-        scanPort(host,portStart,portEnd);
+        for (int port = portStart; port < portEnd; port++) {
+            if (pingHost(host, port, timeout)) {
+                openPorts.add(port);
+            }
+        }
     }
 
+    //So Scene can get the open ports of every scanner
+    public List<Integer> getOpenPorts() {
+        return openPorts;
+    }
 
-    //Pings IP to get response
+    //Pings IP with port to get response -> port open
     public static boolean pingHost(String host, int port, int timeout) {
         try(Socket socket = new Socket()) {
             InetSocketAddress address = new InetSocketAddress(host, port);
             socket.connect(address, timeout); //Tries to connect to the given address on a port, will error out when closed or exceed timeout
             return true;
         } catch (IOException e) {
-            System.err.println("Port " + port + ": " + e.getMessage());
+            //Debug
+            //System.err.println("Port " + port + ": " + e.getMessage());
             return false;
         }
-    }
-
-
-
-    //Main function for scanning
-    public static void scanPort(String host, int portStart, int portEnd, int maxThreads, int[] ignorePort, int timeout) {
-        int[] results = new int[portEnd - portStart + 1];
-        for (int i = portStart; i < portEnd; i++){
-            boolean result = pingHost(host, i, timeout);
-            //If host responded --> port open --> logged in array
-            if(result){
-                results[i-portStart] = i;
-            }
-            //System.out.println(i + ":" + result); //Debug
-        }
-
-        for (int i = 0; i < results.length; i++){
-            if(results[i] != 0){
-                System.out.println(results[i]);
-            }
-
-        }
-    }
-
-    public static void scanPort(String host, int portStart, int portEnd,int maxThreads, int[] ignorePort) {
-        scanPort(host, portStart, portEnd, maxThreads, ignorePort, 200);
-    }
-    public static void scanPort(String host, int portStart, int portEnd, int maxThreads) {
-        scanPort(host, portStart, portEnd, maxThreads, new int[]{});
-    }
-    public static void scanPort(String host, int portStart, int portEnd) {
-        scanPort(host, portStart, portEnd, 0);
-    }
-    public static void scanPort(String host, int portStart) {
-        scanPort(host, portStart, 65535);
-    }
-    public static void scanPort(String host) {
-        scanPort(host, 1);
     }
 }
